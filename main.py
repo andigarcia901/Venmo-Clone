d79b7b116ba486f078253d1d72750a3e0a66e092
andigarcia901 <119568800+andigarcia901@users.noreply.github.com>
2022-12-19 20:34:22 -0600

    User_one and User_two Set Up

---
+++ main.py
@@ -0,0 +1,18 @@
+user_one = {
+    "full name":"Snoopy",
+    "username": "JoeCool", 
+    "password": "password123",
+    "account balance": 1000,
+    "connected banks": [
+        ("Bank of America", 2000)
+    ]
+}
+user_two = {
+    "full name":"Woodstock",
+    "username": "Little Bird", 
+    "password": "bestfriend123",
+    "account balance": 3000,
+    "connected banks": [
+        ("Wells Fargo", 4000)
+    ]
+}
