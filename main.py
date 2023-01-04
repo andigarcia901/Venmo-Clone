64efe67067ba533e31e1689fcc1c0e54c7ce2551
andigarcia901 <119568800+andigarcia901@users.noreply.github.com>
2023-01-04 08:55:18 -1000

    Login Function Set Up

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
 user_one = {
-    "full name":"Snoopy",
-    "username": "JoeCool", 
-    "password": "password123",
-    "account balance": 1000,
-    "connected banks": [
-        ("Bank of America", 2000)
+    'user one account details': [
+        {
+            "full name" : "Snoopy",
+            "username" : "JoeCool",
+            "password" : "password123",
+            "account balance" : 100,
+            "connected banks": [
+                ("Bank of America", 200)
+            ]
+        
+        }
     ]
 }
 user_two = {
-    "full name":"Woodstock",
-    "username": "Little Bird", 
-    "password": "bestfriend123",
-    "account balance": 3000,
-    "connected banks": [
-        ("Wells Fargo", 4000)
+    'user two account details': [
+        {
+            "full name" : "Woodstock",
+            "username" : "LittleBird",
+            "password" : "bestfriend123",
+            "account balance" : 150,
+            "connected banks" :[
+                ("Wells Fargo", 250)
+            ]
+        }
     ]
 }
+
+def get_username():
+    username = input("Please enter username: ")
+    if username == ['user one account details'][1]['username']:
+        print("Username is correct.")
+    else:
+        print("Username is incorrect.")
+
+get_username()
+
+def get_password():
+    password = input("Please enter passowrd: ")
+    if password == user_one ["user one account details"][2]['password']:
+        print("You have successfully logged in.")
+    else:
+        print("Password is incorrect.")
+
+get_password()
+print('')
+print(f'Your account balance is ')
+
+
+
+
